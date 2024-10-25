2b97db6833f38ce5ae503d5fdd03331ffad70128
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-10-25 00:43:21 -0300

    feat: delete bit posts

--- src/components/BitPost/BitPost.jsx
+++ src/components/BitPost/BitPost.jsx
@@ -4,8 +4,13 @@ import CardContent from "@mui/material/CardContent";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 import { red } from "@mui/material/colors";
+import BitPostActions from "./BitPostActions";
+import axios from "axios";
+import { BitContext } from "../../context/BitsContext";
+import { useContext } from "react";
 
-function BitPost({ bit }) {
+function BitPost({ Bit }) {
+  const { bit, setBit } = useContext(BitContext);
   const dateFormat = (timestamp) => {
     const date = new Date(timestamp);
     return date.toLocaleString("pt-br");
@@ -13,24 +18,35 @@ function BitPost({ bit }) {
   const nameInitialsFormat = (name) => {
     return name.split("")[0].toUpperCase();
   };
+  const handleDelete = async (id) => {
+    await axios.delete(
+      `https://api-blissfields-997949264503.southamerica-east1.run.app/bits/${id}`
+    );
+    setBit(bit.filter((bit) => Bit.id !== id));
+  };
 
   return (
-    <Card
-      variant="elevation"
-      sx={{ maxWidth: 400, width: "100%"  }}
-    >
+    <Card variant="elevation" sx={{ maxWidth: 400, width: "100%" }}>
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            {nameInitialsFormat(bit.user)}
+            {nameInitialsFormat(Bit.user)}
           </Avatar>
         }
-        title={bit.user}
-        subheader={dateFormat(bit.timestamp)}
+        action={
+          <BitPostActions
+            handleDelete={() => {
+              handleDelete(Bit.id);
+            }}
+            handleEdit
+          />
+        }
+        title={Bit.user}
+        subheader={dateFormat(Bit.timestamp)}
       />
       <CardContent>
         <Typography variant="body1" sx={{ color: "text.secondary" }}>
-          {bit.text}
+          {Bit.text}
         </Typography>
       </CardContent>
     </Card>
